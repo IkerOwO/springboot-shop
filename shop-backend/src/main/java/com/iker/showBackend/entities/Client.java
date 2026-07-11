ed215d82c067dfd1b7c7a4e842fda6f1c0897c34
Iker <162556194+IkerOwO@users.noreply.github.com>
2026-07-11 18:20:24 +0200

    Backend con springboot acabado

--- shop-backend/src/main/java/com/iker/showBackend/entities/Client.java
+++ shop-backend/src/main/java/com/iker/showBackend/entities/Client.java
@@ -14,67 +14,67 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String client_name;
-    private String client_surname;
-    private String client_password;
-    private String client_phone;
-    private String client_email;
+    private String clientName;
+    private String clientSurname;
+    private String clientPassword;
+    private String clientPhone;
+    private String clientEmail;
 
     public Client(){}
 
-    public Client(String client_name, String client_surname, String client_password, String client_phone, String client_email) {
-        this.client_name = client_name;
-        this.client_surname = client_surname;
-        this.client_password = client_password;
-        this.client_phone = client_phone;
-        this.client_email = client_email;
+    public Client(String clientName, String clientSurname, String clientPassword, String clientPhone, String clientEmail) {
+        this.clientName = clientName;
+        this.clientSurname = clientSurname;
+        this.clientPassword = clientPassword;
+        this.clientPhone = clientPhone;
+        this.clientEmail = clientEmail;
     }
 
     public Long getId() {
         return id;
     }
 
-    public String getClient_password() {
-        return client_password;
-    }
-
-    public void setClient_password(String client_password) {
-        this.client_password = client_password;
-    }
-
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getClient_name() {
-        return client_name;
+    public String getClientName() {
+        return clientName;
     }
 
-    public void setClient_name(String client_name) {
-        this.client_name = client_name;
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
     }
 
-    public String getClient_surname() {
-        return client_surname;
+    public String getClientSurname() {
+        return clientSurname;
     }
 
-    public void setClient_surname(String client_surname) {
-        this.client_surname = client_surname;
+    public void setClientSurname(String clientSurname) {
+        this.clientSurname = clientSurname;
     }
 
-    public String getClient_phone() {
-        return client_phone;
+    public String getClientPassword() {
+        return clientPassword;
     }
 
-    public void setClient_phone(String client_phone) {
-        this.client_phone = client_phone;
+    public void setClientPassword(String clientPassword) {
+        this.clientPassword = clientPassword;
     }
 
-    public String getClient_email() {
-        return client_email;
+    public String getClientPhone() {
+        return clientPhone;
     }
 
-    public void setClient_email(String client_email) {
-        this.client_email = client_email;
+    public void setClientPhone(String clientPhone) {
+        this.clientPhone = clientPhone;
     }
+
+    public String getClientEmail() {
+        return clientEmail;
+    }
+
+    public void setClientEmail(String clientEmail) {
+        this.clientEmail = clientEmail;
+    } 
 }
